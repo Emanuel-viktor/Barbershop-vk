188fbf7775c40fe911632ff0f1e98107093d27d2
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2026-07-30 19:37:53 -0300

    first commit

---
+++ Back-end/src/test/java/barbershop_vk/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package barbershop_vk.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
