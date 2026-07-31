a9504e0af963317c9410a7bb89035a739cf22d4c
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2026-07-30 21:10:11 -0300

    User entity

--- Back-end/src/main/java/barbershop_vk/entity/User.java
+++ Back-end/src/main/java/barbershop_vk/entity/User.java
@@ -1,4 +1,75 @@
 package barbershop_vk.entity;
 
+import java.util.Objects;
+
 public class User {
+
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String telephone;
+
+    public User() {
+    }
+
+    public User(int id, String name, String email, String password, String telephone) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
