edc03c7db56fc08ea48ecd6a6729c1225835da7e
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2026-07-30 21:01:04 -0300

    barber entity

--- Back-end/src/main/java/barbershop_vk/entity/Barber.java
+++ Back-end/src/main/java/barbershop_vk/entity/Barber.java
@@ -1,4 +1,84 @@
 package barbershop_vk.entity;
 
+import java.util.Objects;
+
 public class Barber {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private String telephone;
+    private String description;
+
+    public Barber() {
+    }
+
+    public Barber(Long id, String name, String email, String password, String telephone, String description) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.telephone = telephone;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Barber barber = (Barber) o;
+        return Objects.equals(id, barber.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
