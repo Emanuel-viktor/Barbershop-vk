442d93c8f3c2b005e124b1da355e8d7c17b1d09a
Emanuel Viktor <emanuel.bol.cco@gmail.com>
2026-09-21 11:06:46 -0300

    Scheduling JWT

--- Back-end/src/main/java/barbershop_vk/controller/SchedulingController.java
+++ Back-end/src/main/java/barbershop_vk/controller/SchedulingController.java
@@ -5,6 +5,7 @@ import barbershop_vk.repository.SchedulingRepository;
 import barbershop_vk.service.SchedulingService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
@@ -16,6 +17,7 @@ public class SchedulingController {
     private SchedulingService schedulingService;
 
     @GetMapping
+    @PreAuthorize("hasRole('BARBEIRO')")
     public List<Scheduling> findAll() {
         return schedulingService.findAll();
     }
